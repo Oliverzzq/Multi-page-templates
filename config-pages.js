b5f62dbd92a71edd86302d628b9e1b2ad6e9c7a1
WQK <316053557@qq.com>
2020-05-22 17:45:55 +0800

    fir

--- config-pages.js
+++ config-pages.js
@@ -1,8 +1,8 @@
-// 入口文件配置
+// entry
+// CommonsChunkPlugin
+
 
 module.exports = {
     index: './src/js/index.js',
     about: './src/js/about.js'
-    // CommonsChunkPlugin
-    // common: ['jquery', 'vue']
 }
